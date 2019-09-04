c1321f447b7a3f5ce95a7f245599e197066effb8
Надежда Морозова <Nadia-0012@mail.ru>
2019-09-04 10:41:51 +0300

    Initial Commit

---
+++ MyNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyNews
+//
+//  Created by Надежда Морозова on 04/09/2019.
+//  Copyright © 2019 Надежда Морозова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
